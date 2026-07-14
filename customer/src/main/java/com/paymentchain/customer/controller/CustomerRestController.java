5dabec4ff14ef54224fe366b55efef149aea9019
jmarchesini96 <jeancarlo.marchesini96@gmail.com>
2026-07-14 15:41:07 -0500

    feat(billing): implementar CRUD de facturación y configurar Swagger

--- customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
+++ customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
@@ -3,7 +3,6 @@ package com.paymentchain.customer.controller;
 import com.paymentchain.customer.entities.Customer;
 import com.paymentchain.customer.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
